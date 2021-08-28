7384b23ea55fa36510b101a0bd4fee789fe0f283
fabio leao <fabioleao48@gmail.com>
2021-08-28 18:14:55 -0300

    files deleted

--- README.md
+++ README.md
@@ -1,4 +1,1 @@
 # THM-Card
-Simple Tryhackme card api that uses user badge id to generate a visual card with users info.
-
-WIP.
--- index.js
+++ index.js
@@ -1,31 +1,25 @@
 const express = require('express')
 const {spawn} = require('child_process');
-const xml = require('xml');
+
 
 const app = express()
 const port = 5000
+
 app.get('/card', (req, res) => {
- 
- var dataToSend;
- var doc;
- const id = req.query.id
- 
- // spawn new child process to call the python script
- const python = spawn('python', ['script1.py', id]);
- // collect data from script
- python.stdout.on('data', function (data) {
-  console.log('Pipe data from python script ...');
-  dataToSend = data.toString();
- });
- // in close event we are sure that stream from child process is closed
- python.on('close', (code) => {
- console.log(`child process close all stdio with code ${code}`);
- // send data to browser
- console.log(dataToSend)
- res.set('Content-Type', 'image/svg+xml; charset=utf-8')
- res.send(dataToSend)
- });
- 
+
+    var dataToSend;
+    const id = req.query.id
+    const python = spawn('python', ['script1.py', id])
+    python.stdout.on('data', function (data) {
+        console.log('Pipe data from python script ...')
+        dataToSend = data.toString();
+    })
+    python.on('close', (code) => {
+        console.log('child process close all stdio with code ${code}')
+        console.log(dataToSend)
+        res.set('Content-Type', 'image/svg+xml; charset=utf-8')
+        res.send(dataToSend)
+    })
+
 })
-app.listen(process.env.PORT || port, () => console.log(`Example app listening on port 
-${port}!`))
+app.listen(process.env.PORT || port, () => console.log('Example app listening on port ${port}!'))
